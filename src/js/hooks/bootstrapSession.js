1a74bcf6a8035076e0e00a8b48d88284ced10aec
cvaz007 <sebastianmoreno301177@correo.itm.edu.co>
2024-01-31 09:39:57 -0500

    validaciones globales

--- src/js/hooks/bootstrapSession.js
+++ src/js/hooks/bootstrapSession.js
@@ -1,15 +1,11 @@
 (() => {
-  //Get the user section of localStorage
   const isAuthorized = localStorage.getItem("isAuthorized");
-  //Get the route where the user wants to access
   const path = window.location.pathname;
-  //Cut the path for allow just the file that user is trying to access
-  const routeActive = path.substring(path.lastIndexOf("/") + 1);
-  console.log(routeActive);
-  //Create a list with the name of all list that I want protect
-  const privateRoutes = ["administrator.html"];
+  const routeActive = path.substring(path.at(-2)("/") + 1);
 
-  //If the current route is in the list of others protect routes and it's not  authorized, so this redirects to Login
+
+  console.log(routeActive);
+  const privateRoutes = ["admin/index.html","coder/index.html","trainer/index.html"];
   console.log(privateRoutes.includes(routeActive));
 
   if (privateRoutes.includes(routeActive) && !isAuthorized) {
