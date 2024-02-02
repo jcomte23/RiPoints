dc27a1d6f516bdffc7f052e54054fd36e4da9c89
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-02 10:12:11 -0500

    avances en la mejora del codigo

--- src/js/hooks/bootstrapSession.js
+++ src/js/hooks/bootstrapSession.js
@@ -1,14 +1,37 @@
 (() => {
-  const isAuthorized = localStorage.getItem("isAuthorized");
   const path = window.location.pathname;
-  const routeActive = path.substring(path.at(-2)("/") + 1);
+  // const routeActive = path.substring(path.at(-2)("/") + 1);
+  // let privateRoutes
+
+  // const isAuthorized = localStorage.getItem("isAuthorized");
+  // const user = localStorage.getItem("userStorage")
+  // let userOnline
+  // if (user) {
+  //   userOnline = JSON.parse(user)
+  // } else {
+  //   window.location.href = "/";
+  // }
+
+  // switch (userOnline.role.name) {
+  //   case "trainer":
+  //     privateRoutes = ["admin/index.html"];
+  //     if (privateRoutes.includes(routeActive) && !isAuthorized) {
+  //       window.location.href = "../index.html";
+  //     }else{
+  //       window.location.href = "../index.html";
+  //     }
+  //     break;
+
+  //   default:
+  //     break;
+  // }
 
 
-  console.log(routeActive);
-  const privateRoutes = ["admin/index.html","coder/index.html","trainer/index.html"];
-  console.log(privateRoutes.includes(routeActive));
 
-  if (privateRoutes.includes(routeActive) && !isAuthorized) {
-    window.location.href = "../index.html";
-  }
+
+
+  // const privateRoutes = ["admin/index.html", "coder/index.html", "trainer/index.html"];
+  // console.log(privateRoutes.includes(routeActive));
+
+
 })();
