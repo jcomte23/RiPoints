f19f4198adec362f922dcb45a720819db229b447
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-02 13:30:11 -0500

    avances en el guardian

--- src/js/hooks/bootstrapSession.js
+++ src/js/hooks/bootstrapSession.js
@@ -1,37 +1,8 @@
 (() => {
-  const path = window.location.pathname;
-  // const routeActive = path.substring(path.at(-2)("/") + 1);
-  // let privateRoutes
-
-  // const isAuthorized = localStorage.getItem("isAuthorized");
-  // const user = localStorage.getItem("userStorage")
-  // let userOnline
-  // if (user) {
-  //   userOnline = JSON.parse(user)
-  // } else {
-  //   window.location.href = "/";
-  // }
-
-  // switch (userOnline.role.name) {
-  //   case "trainer":
-  //     privateRoutes = ["admin/index.html"];
-  //     if (privateRoutes.includes(routeActive) && !isAuthorized) {
-  //       window.location.href = "../index.html";
-  //     }else{
-  //       window.location.href = "../index.html";
-  //     }
-  //     break;
-
-  //   default:
-  //     break;
-  // }
-
-
-
-
-
-  // const privateRoutes = ["admin/index.html", "coder/index.html", "trainer/index.html"];
-  // console.log(privateRoutes.includes(routeActive));
-
-
+  const user = localStorage.getItem("userStorage")
+  if (user) {
+    userOnline = JSON.parse(user)
+  } else {
+    window.location.href = "/";
+  }
 })();
--- src/js/translator.js
+++ src/js/translator.js
@@ -15,7 +15,7 @@ i18next
   .use(Backend)
   .init({
     lng: language,
-    debug: true,
+    debug: false,
     backend: {
       loadPath: "/locales/{{lng}}/{{ns}}.json",
     },
