701abf3165216be969de14236ea79cbc7af62245
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-03 11:42:59 -0500

    implementacion de seguridad a las rutas

--- src/js/hooks/bootstrapSession.js
+++ src/js/hooks/bootstrapSession.js
@@ -1,8 +1,22 @@
 (() => {
+  let userOnline
+  const urlCompleta = window.location.href;
   const user = localStorage.getItem("userStorage")
-  if (user) {
+  const authorized = JSON.parse(localStorage.getItem("isAutorizated"))
+  if (user && authorized===true) {
     userOnline = JSON.parse(user)
   } else {
     window.location.href = "/";
   }
-})();
+
+  if (!urlCompleta.includes(userOnline.role.name) && userOnline.role.name==="admin") {
+    window.location.href = "/src/pages/admin/index.html"
+    alert("ho autorizado")
+  } else if(!urlCompleta.includes(userOnline.role.name) && userOnline.role.name==="trainer"){
+    window.location.href = "/src/pages/trainer/index.html"
+    alert("ho autorizado")
+  } else if(!urlCompleta.includes(userOnline.role.name) && userOnline.role.name==="coder"){
+    window.location.href = "/src/pages/coder/index.html"
+    alert("ho autorizado")
+  }
+})()
