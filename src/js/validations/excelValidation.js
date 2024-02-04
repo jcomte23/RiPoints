58084f4f66e57b017143745a2b37e8cb4424c56e
cristianbyte <charlsmaritz@gmail.com>
2024-02-04 01:53:25 -0500

    admit multiple files

--- src/js/validations/excelValidation.js
+++ src/js/validations/excelValidation.js
@@ -1,19 +1,34 @@
 import * as ExcelJS from 'exceljs';
 
-export function handleFileSelect(fileInput = false, callback) {
-    let file;
+let file;
+const allowedExtensions = ['xlsx'];
 
-    if (fileInput instanceof DataTransfer) {
-        // If a DataTransfer object is provided, take the file from the object
-        file = fileInput.files[0];
-    } else {
-        // If a DataTransfer object is not provided, get the file from the INPUT
-        file = document.getElementById('formFile').files[0];
+export function handleFileSelect(fileInput = false, callback) {
+
+
+    function processFiles(files, callback) {
+        // Iterate over each file in the list
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            handleSingleFile(file, callback);
+        }
     }
 
+    // If a DataTransfer object is provided, take the file from the object
+    if (fileInput instanceof DataTransfer) {
+        const files = fileInput.files;
+        processFiles(files,callback)
+        
+    } else {
+        // If a DataTransfer object is not provided, get the file from the INPUT
+        const files = document.getElementById('formFile').files;
+        processFiles(files,callback)
+        
+    }
+}
 
+function handleSingleFile(file, callback) {
     if (file) {
-        const allowedExtensions = ['xlsx'];
         const fileExtension = file.name.split('.').pop().toLowerCase();
 
        // Validate the file type
@@ -26,27 +41,33 @@ export function handleFileSelect(fileInput = false, callback) {
         const reader = new FileReader();
 
         reader.onload = function (e) {
-            const data = new Uint8Array(e.target.result);
-
-            const workbook = new ExcelJS.Workbook();
-            workbook.xlsx.load(data).then(function () {
-                let sheets = [];
-                
-                // You can access the sheets and cells of the Excel file here
-                workbook.eachSheet(function (worksheet, sheetId) {
-                    sheets.push({ sheetId, sheetName: worksheet.name, data: worksheet.getSheetValues() });
+            try{
+                const data = new Uint8Array(e.target.result);
+    
+                const workbook = new ExcelJS.Workbook();
+                workbook.xlsx.load(data).then(function () {
+                    let sheets = [];
+                    
+                    // You can access the sheets and cells of the Excel file here
+                    workbook.eachSheet(function (worksheet, sheetId) {
+                        sheets.push({ sheetId, sheetName: worksheet.name, data: worksheet.getSheetValues() });
+                    });
+    
+                    callback(sheets);
+                    
+                    // LOAD SUCCESSFULL
+                    const imageIdicator = document.querySelector('#labelFile figure img')
+                    imageIdicator.src = '/icons/file_uploaded.svg'
+                    document.querySelector('#labelFile h3').textContent = '¡SUCCESS!'
                 });
 
-                callback(sheets);
-            });
+            }catch{
+                console.error('Error loading the file:', error);
+                alert('There was an error loading the file. Make sure it is a valid Excel file.');
+            }
         };
-
+        // converting format
         reader.readAsArrayBuffer(file);
-
-        // LOAD SUCCESSFULL
-        const imageIdicator = document.querySelector('#labelFile figure img')
-        imageIdicator.src = '/icons/file_uploaded.svg'
-        document.querySelector('#labelFile h3').textContent = '¡SUCCESS!'
 
     }
 }
