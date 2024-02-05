e80bed022b265c3c4cbd29623233fe073e1aaa45
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-05 08:23:08 -0500

    creacion del boton para destruir la sesion

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 import * as bootstrap from 'bootstrap'
 import '../scss/style.scss'
 
+const btnLogout=document.getElementById("btn-logout")
+
+btnLogout.addEventListener('click',logout)
+
+function logout() {
+    localStorage.setItem("userStorage", "");
+    localStorage.setItem("isAutorizated", "false")
+    window.location.href = "/"
+}
