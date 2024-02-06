0492a731a1075ffedad8adbcb03d87520dd7ab71
areviRPC <csebastian.riverap@gmail.com>
2024-02-06 12:54:27 -0500

    ranking table updated

--- src/js/services/helpers.js
+++ src/js/services/helpers.js
@@ -16,3 +16,16 @@ export async function updateClanPoints(){
     // Sorting the ranking by points in descending order !!! IT'S BETTER TO SORT BY THE REQUEST, HOW?: this way: 'clans?_sort=-points'
     // const sortedClans = Object.values(clans).sort((a, b) => b.points - a.points);
 }
+
+export function setImageMultiple(selector,imageList,imagesExist = false){
+    let listSelected = document.querySelectorAll(selector)
+    if(imagesExist){
+        //sehace un style.backgroundImage = ulr('imageList[i]')
+        return 0
+    }else{
+        listSelected.forEach((el, index)=>{
+            el.classList.add('fancyLetter')
+            el.textContent = imageList[index].charAt(0)
+        })
+    }
+}
