6d79406dfa40085517f447ebe03ab238b5a921e5
areviRPC <csebastian.riverap@gmail.com>
2024-02-06 07:58:12 -0500

    sorting by the request on clans

--- src/components/clans.js
+++ src/components/clans.js
@@ -1,29 +1,26 @@
 import { updateContent } from "../js/translator";
 import { ranking } from "./ranking"
 
-export const showClans = (element) => {
+export const showClans = async (element) => {
     element.innerHTML = `
         <section class="p-1 clans-table">
-        <article class="bg-secondary rankings-clanes text-center text-white px-4">
-            <h1 data-i18n="clan_ranking" class="text-capitalize">clasificación de clanes</h1>
-        </article>
-        <article class="table-responsive">
-            <table class="table table-borderless">
-                <thead>
-                    <tr>
-                        <th class="text-center col-3"><button type="button" class="btn btn-primary rounded-pill w-100">Posicion</button></th>
-                        <th class="text-center col-7"><button type="button" class="btn btn-primary rounded-pill w-100">Clan</button></th>
-                        <th class="text-center col-3"><button type="button" class="btn btn-primary rounded-pill w-100">Puntos</button></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="text-center col-3"><button type="button" class="btn btn-secondary rounded-pill w-100">1</button></td>
-                        <td class="text-center col-7"><button type="button" class="btn btn-secondary rounded-pill w-100">meta</button></td>
-                        <td class="text-center col-3"><button type="button" class="btn btn-secondary rounded-pill w-100">100</button></td>
-                    </tr>
-                </tbody>
-            </table>
+            <article class="bg-secondary rankings-clanes text-center text-white px-4">
+                <h1 data-i18n="clan_ranking" class="text-capitalize">clasificación de clanes</h1>
+            </article>
+            <article class="table-responsive">
+                <table class="table table-borderless">
+                    <thead>
+                        <tr>
+                            <th class="text-center col-3"><button type="button" class="btn btn-primary rounded-pill w-100">Posicion</button></th>
+                            <th class="text-center col-7"><button type="button" class="btn btn-primary rounded-pill w-100">Clan</button></th>
+                            <th class="text-center col-3"><button type="button" class="btn btn-primary rounded-pill w-100">Puntos</button></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${await ranking('clans?_sort=-points')}
+                    </tbody>
+                </table>
+           </article> 
         </section>
     `
  updateContent()
--- src/components/listTr.js
+++ src/components/listTr.js
@@ -1,17 +1,19 @@
 import { get } from '../js/services/helpers'
+import { updateContent } from '../js/translator';
 
 export async function loadCodersTr(element) {
   let list = await get('users?roleId=3');
 
   list.forEach((coder) => {
     element.innerHTML += `
-    <tr>
-      <td>${coder.name}</td>
-      <td>${coder.lastName}</td>
-      <td>${coder.clanId}</td>
-      <td>${Object.entries(coder.day_point).reduce((acc, curr) => Number(curr[1]) + acc, 0)}</td>
-      <td><button class="edit btn btn-primary text-capitalize" data-i18n="edit" data-bs-toggle="modal" data-bs-target="#exampleModal"></button></td>
-    </tr>
+      <tr>
+        <td>${coder.name}</td>
+        <td>${coder.lastName}</td>
+        <td>${coder.clanId}</td>
+        <td>${Object.entries(coder.day_point).reduce((acc, curr) => Number(curr[1]) + acc, 0)}</td>
+        <td><button class="edit btn btn-primary text-capitalize" data-i18n="edit" data-bs-toggle="modal" data-bs-target="#exampleModal"></button></td>
+      </tr>
     `;
   });
+  updateContent();
 }
