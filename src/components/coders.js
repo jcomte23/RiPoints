0ec01600bb065ef98b2ee172bb651bede29fbd2e
cristianbyte <charlmaritz@gmail.com>
2024-02-07 02:02:35 -0500

    update(coder list)

--- src/components/coders.js
+++ src/components/coders.js
@@ -12,10 +12,15 @@ export function showCoders(element) {
     <div class="listCoders">
       <div class="listCoders__header" >
         <h2 data-i18n="developers" ></h2>
-        <input class="my-2 search" type="text" name="" id="searchKeywords" placeholder="Search..." />
+        <div class="search" >
+          <input class="my-2" type="text" name="" id="searchKeywords" i18n-placeholder="developers" />
+          <svg class="search__icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+          </svg>
+        </div>
       </div>
        
-      <table id="myTable" class="table mt-3">
+      <table id="myTable" class="listCoders__table">
         <thead>
           <tr>
             <th data-i18n="name" ></th>
