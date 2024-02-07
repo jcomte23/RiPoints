c9fff618aa9e80bf9692f7ca5fb943b30a4e6b39
cristianbyte <charlsmarit@gmail.com>
2024-02-07 12:21:41 -0500

    update

--- src/components/listTr.js
+++ src/components/listTr.js
@@ -1,17 +1,18 @@
 import { get } from '../js/services/helpers'
 import { updateContent } from '../js/translator';
-import { calcScoreCoins } from '../js/services/getWinCoins';
+import { calcScoreCoins, calcWinCoins } from '../js/services/getWinCoins';
 
 export async function loadCodersTr(element) {
-  let list = await get('users?rolId=3&_embed=clan&_embed=scoreCoins');
+  let list = await get('users?rolId=3&_embed=clan&_embed=scoreCoins&_embed=winCoins');
 
   list.forEach((coder) => {
+    let add = calcWinCoins(coder.winCoins);
     element.innerHTML += `
       <tr>
         <td>${coder.name}</td>
         <td>${coder.lastName}</td>
         <td>${coder.clan.name}</td>
-        <td>${calcScoreCoins(coder.scoreCoins)}</td>
+        <td data-plus=${add == 0 ? '' :add > 0 ? `"+${add}" class="plus"`:`"${add}" class="less"` } >${calcScoreCoins(coder.scoreCoins)}</td>
         <td><button class="edit btn btn-primary text-capitalize" data-i18n="edit" data-bs-toggle="modal" data-bs-target="#exampleModal"></button></td>
       </tr>
     `;
