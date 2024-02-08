abbaa6ca3336888dc2fb06088cae5253404b57f8
cristianbyte <charlsmaritz@gmail.com>
2024-02-08 17:49:19 -0500

    coder

--- src/components/coder.js
+++ src/components/coder.js
@@ -1,3 +1,4 @@
+import { setImageMultiple } from "../js/services/helpers";
 import { changeLanguageOnClick, updateContent } from "../js/translator";
 import { historyWinCoinsByUserId } from "../js/usecases/winCoinsHistory";
 
@@ -43,6 +44,7 @@ export const renderCoder = (element) => {
   document.querySelectorAll('.coder__name').forEach((el) => {
     el.textContent = user.name + ' ' + user.lastName 
   })
+  setImageMultiple('.coder__profile__clanShiled',[user.clanId,user.clanId])
   updateContent();
   historyCoderRender(user);
 }
