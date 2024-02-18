6dd4f6fe681be2b31df1f9dd9eeb64064958dc4b
Sebastian Moreno E <92068938+Cvaz007@users.noreply.github.com>
2024-02-18 14:00:47 -0500

    modal completamente dinamico

--- src/js/usecases/setCoins.js
+++ src/js/usecases/setCoins.js
@@ -5,17 +5,16 @@ import { saveExtraCoins } from "../services/saveExtraCoins";
 export const setCoinsByUser = async (coins) => {
   const user = JSON.parse(localStorage.getItem("userStorage"));
   const coderId = localStorage.getItem("coderEdit");
-  const coder = await getUserById(coderId);
+  //const coder = await getUserById(coderId);
   const comment = document.getElementById("observations").value;
   const date = document.getElementById("fecha").value;
   const data = {
     coins,
     comment,
     user: { name: `${user.name} ${user.lastName}`, id: user.id },
-    coderId: localStorage.getItem("coderEdit"),
+    coderId: coderId,
     date:date
   };
-  console.log(data);
   await saveExtraCoins(data);
   // await updateClansPoints(coder[0].clanId);
   setTimeout(() => {
